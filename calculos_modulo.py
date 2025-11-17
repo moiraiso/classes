f9770a2cbd627fd68166a23b082aa0380c607a24
moiraiso <gabriel.vpr7@gmail.com>
2025-11-17 20:14:00 -0300

    Update and rename calculos_classe.py to calculos_modulo.py

--- calculos_modulo.py
+++ calculos_modulo.py
@@ -1,12 +1,12 @@
-import classe_teste as ct
+import modulo_teste as mt
 
 alunas = ["Maria", "Ana", "Joana", "Carla", "Paula"]
 notas = [7.5, 8.0, 6.0, 9.0, 5.5]
 
 # Utilizando a classe:
 # Calculando a média:
-resultado = ct.calcular_media(notas)
+resultado = mt.calcular_media(notas)
 print(f"A média da turma é {resultado}")
 # Aluna com a maior nota:
-aluna, nota_maxima = ct.maior_nota(notas,alunas)
+aluna, nota_maxima = cm.maior_nota(notas,alunas)
 print(f"A maior nota foi {nota_maxima}, da aluna {aluna}")
